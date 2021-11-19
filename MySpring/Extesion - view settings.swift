6285efc062f170b1591fbb595225c5ff745568ca
Kirill Ki <kirillkalapov@gmail.com>
2021-11-19 14:52:57 +0100

    Animation done, exept indication last animation name

---
+++ MySpring/Extesion - view settings.swift
@@ -0,0 +1,14 @@
+//
+//  Extesion - view settings.swift
+//  MySpring
+//
+//  Created by  Mr.Ki on 19.11.2021.
+//
+
+import UIKit
+
+
+extension ViewController {
+    
+    
+}
--- MySpring/ViewController.swift
+++ MySpring/ViewController.swift
@@ -5,15 +5,105 @@
 //  Created by  Mr.Ki on 18.11.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    
+    var animationCont = 0
+    var currentAnimation = "shake"
+    
+    
+    var animationTypes = [
+                          "shake",
+                          "pop",
+                          "morph",
+                          "squeeze",
+                          "wobble",
+                          "swing",
+                          "flipX",
+                          "flipY",
+                          "fall",
+                            "squeezeLeft",
+                            "squeezeRight",
+                            "squeezeDown",
+                            "squeezeUp",
+                            "slideLeft",
+                            "slideRight",
+                            "slideDown",
+                            "slideUp",
+                            "fadeIn",
+                            "fadeOut",
+                            "fadeInLeft",
+                            "fadeInRight",
+                            "fadeInDown",
+                            "fadeInUp",
+                            "zoomIn",
+                            "zoomOut",
+                            "flash",
+    ]
+    
+    
+    
+    @IBOutlet weak var button: SpringButton!
+    
+    
+    @IBOutlet weak var viewOutlet: SpringView!
+    @IBOutlet weak var textViewOutlet: SpringLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor.systemTeal.cgColor,
+            UIColor.systemGray
+                .cgColor
+        ]
+  
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
 
+    @IBAction func buttonTapped(_ sender: SpringButton) {
+       
+        
+        
+        if animationTypes[animationCont] != animationTypes.last {
+            currentAnimation = animationTypes[animationCont]
+            viewOutlet.animation = currentAnimation
+            viewOutlet.curve = "easeIn"
+            viewOutlet.force = 2
+            viewOutlet.duration = 1
+            viewOutlet.delay = 0.3
+            viewOutlet.animate()
+          
+            animationCont += 1
+            
+            
+            
+        } else {
+            
+            viewOutlet.animation = "flash"
+            viewOutlet.curve = "easeIn"
+            viewOutlet.force = 2
+            viewOutlet.duration = 1
+            viewOutlet.delay = 0.3
+            viewOutlet.animate()
+            
+            animationCont = 0
+        }
+        
+    
+        button.setTitle("Next \(animationTypes[animationCont])", for: .normal)
+        textViewOutlet.text = currentAnimation
+        
+        print(animationTypes[animationCont])
+        
+    }
+    
+   
+    
 }
+
+
 
